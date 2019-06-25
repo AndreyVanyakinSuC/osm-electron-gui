397a22576c952b130d76d4163cb9552c4440ce64
AndreyVanyakin <andrey.vanyakin@gmail.com>
2019-06-25 12:56:14 +0300

    Better popups for towers wo sensors

--- main-window/src/components/Fresh/Map/Tower/TowerEmptyPopup.js
+++ main-window/src/components/Fresh/Map/Tower/TowerEmptyPopup.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import CopyCoordinatesBtn from './CopyCoordinatesBtn';
 import { Popup } from 'react-leaflet';
 
 const TowerEmptyPopup = ({ towerID, schema }) => {
 
-    const {number, Name} = schema.towers[towerID];
+    const {number, Name, coordinates} = schema.towers[towerID];
 
-    return (<Popup className='tower-popup'>
-        <div> оп. №{number} <br/> типа {Name} </div>
-    </Popup>);
+    return (
+        <Popup className='tower-popup'>
+            <div className='tower-popup-header empty'> 
+                <div className = 'tower-nums'>
+                    <div className='tower-number'>
+                            <span className='unit'>№ </span>
+                            <span>{number}</span>
+                            <CopyCoordinatesBtn coordinates={coordinates}/>
+                            <div className='unit'>{Name}</div>
+                    </div>
+                </div>
+            </div>
+        </Popup>);
 }
  
 export default TowerEmptyPopup;
