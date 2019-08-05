cad36a42ca489f04cda677d54c389ee236e3110f
Vanyakin Andrey <vanyakin@Lenovo_STE.opten.ru>
2019-08-05 15:44:16 +0300

    Keep alive and pings TBT

--- Electron/source.js
+++ Electron/source.js
@@ -19,78 +19,70 @@ const notifier = require('./notifier');
 
 let es;
 
+const keepaliveSecs = 20;
+let keepaliveTimer = null;
+
 // Will be updated by onerror and reset by onopen or by onclose
 let errCount = 0;
 
 // Connect to server
 const connect = async () => {
-  // 1) Read settings
-  const server = new URL(readSettings().url);
+  try {
+    // 1) Read settings
+    const server = new URL(readSettings().url);
 
-  // 2) TCP ping ip:port
-  log.info(
-    `[COM] Will check if ${server.hostname}:${server.port} responds to TCP pings`
-  );
-  const isAvailable = await tcpPing(server.hostname, parseInt(server.port));
+    // 2) TCP ping ip:port
+    log.info(
+      `[COM] Will check if ${server.hostname}:${server.port} responds to TCP pings`
+    );
+    const isAvailable = await tcpPing(server.hostname, parseInt(server.port));
 
-  if (isAvailable) {
-    // 3) Establish sse connection
-    log.info(`[SSE] TCP ping @ ${server.hostname} suceeded`);
-    notifier.emit(PING_OK);
-    server.pathname = '/sse';
+    if (isAvailable) {
+      // 3) Establish sse connection
+      log.info(`[SSE] TCP ping @ ${server.hostname} suceeded`);
+      notifier.emit(PING_OK);
+      server.pathname = '/sse';
 
-    es = new EventSource(server.href);
+      es = new EventSource(server.href);
 
-    if (es.readyState === 0) {
-      log.info(`[SSE] Connecting to ${es.url}..`);
-      notifier.emit(CONNECTING, es.url);
-    }
-
-    es.onopen = () => {
-      errCount = 0;
-      log.info(`[SSE] Connected to ${es.url}`);
-      notifier.emit(CONNECTED, es.url);
-    };
-
-    es.onmessage = event => {
-      // log.info(`[SSE] Connected to ${es.url}`)
-      // MANAGE
-    };
-
-    es.onerror = err => {
-      log.error(`[SSE] Error #${errCount} occured` + err.stack);
-      notifier.emit(ISERROR, err.stack);
-
-      const isShouldReconnect = await tcpPing(server.hostname, parseInt(server.port));
-
-      if (isShouldReconnect) {
-        
-        errCount = errCount + 1;
-
-      } else {
-        disconnect();
+      if (es.readyState === 0) {
+        log.info(`[SSE] Connecting to ${es.url}..`);
+        notifier.emit(CONNECTING, es.url);
       }
 
+      es.onopen = () => {
+        errCount = 0;
+        gotActivity();
+        log.info(`[SSE] Connected to ${es.url}`);
+        notifier.emit(CONNECTED, es.url);
 
+        es.addEventListener('schema', schemaJSON => {
+          gotActivity();
+          log.info(`[SSE] Received schema`);
+        });
 
-      // errCount = errCount + 1;
-      // // If it tries to reconenct
-      // if (es.readyState === 0) {
-      //   log.info(`[SSE] Reconnecting..`);
-      //   notifier.emit(RECONNECTING);
-      // }
+        es.addEventListener('fresh', freshJSON => {
+          gotActivity();
+          log.info(`[SSE] Received fresh`);
+        });
 
-      // // if it does not
-      // if (es.readyState === 2) {
-      //   log.info(`[SSE] Disconnected`);
-      //   errCount = 0;
-      //   notifier.emit(ISDISCONNECTED);
-      // }
-    };
-  } else {
-    // Inform log and index
-    log.info(`[SSE] TCP ping @ ${server.hostname} failed`);
-    notifier.emit(PING_NOT_OK, `${server.hostname}`);
+        es.addEventListener('ping', () => {
+          gotActivity();
+          log.info(`[SSE] Received ping`);
+        });
+      };
+
+      es.onerror = err => {
+        log.error(`[SSE] Error #${errCount} occured` + err.stack);
+        notifier.emit(ISERROR, err.stack);
+      };
+    } else {
+      // Inform log and index
+      log.info(`[SSE] TCP ping @ ${server.hostname} failed`);
+      notifier.emit(PING_NOT_OK, `${server.hostname}`);
+    }
+  } catch (error) {
+    log.error(error);
   }
 };
 
@@ -109,5 +101,17 @@ const tcpPing = (ip, portInt) =>
   pify(tcpp.probe)(ip, portInt)
     .then(isAvailable => isAvailable)
     .catch(err => log.error(`[COM] Check failed`, err.stack));
+
+// ACTIVITY TIMER, START EACH TIME A MESSAGE FROM SERVER IS RECEIVED AND LASTS FOR X SECONDS, IF NO MESSAGES DURING SPAN => RECONNECT
+const gotActivity = () => {
+  if (keepaliveTimer != null) {
+    clearTimeout(keepaliveTimer);
+  } else {
+    keepaliveTimer = setTimeout(() => {
+      disconnect();
+      connect();
+    }, keepaliveSecs * 1000);
+  }
+};
 
 module.exports = { es, connect, disconnect };
