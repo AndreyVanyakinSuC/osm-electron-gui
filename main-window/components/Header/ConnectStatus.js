5214fb60c30d136342d01819789747e8a6cbba4c
Andrey Vanyakin <andre@Lenovo_STE.opten.ru>
2020-02-06 14:52:52 +0300

    Blink on new fresh

--- main-window/components/Header/ConnectStatus.js
+++ main-window/components/Header/ConnectStatus.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { displayHuman } from '../../APInHelpers/timeseries';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -6,68 +6,173 @@ import {
   faCircleNotch,
   faTimesCircle
 } from '@fortawesome/free-solid-svg-icons';
+import log from 'electron-log';
 
-const ConnectStatus = ({
-  ip,
-  lastFreshMessageTS,
-  onConnectClick,
-  isConnected,
-  isConnecting,
-  isWaitingHistory,
-  isSchemaAvailable,
-  isFreshAvailable
-}) => {
-  let icon, classes, message, stage, title;
+class ConnectStatus extends PureComponent {
+  state = {
+    lastFreshMessageTS: '',
+    isShouldBlink: true
+  };
 
-  if (isConnected) {
-    if (isWaitingHistory) {
-      icon = <FontAwesomeIcon icon={faCircleNotch} spin />;
-      classes = 'connect-icon connected';
-      message = 'Соединено';
-      title = `IP сервера ${ip}`;
-      stage = 'Запрашиваем архивные данные с сервера..';
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.lastFreshMessageTS !== prevState.lastFreshMessageTS) {
+      return {
+        lastFreshMessageTS: nextProps.lastFreshMessageTS,
+        isShouldBlink: true
+      };
     } else {
-      icon = <FontAwesomeIcon icon={faCircle} />;
-      classes = 'connect-icon connected';
-      message = 'Соединено';
-      title = `IP сервера ${ip}   Последнее сообщение получено в ${displayHuman(
-        lastFreshMessageTS
-      )}`;
-      stage = '';
-    }
-  } else if (!isConnected) {
-    if (isConnecting) {
-      icon = <FontAwesomeIcon icon={faCircleNotch} spin />;
-      classes = 'connect-icon connecting';
-      message = 'Соединяемся..';
-      title = `IP сервера ${ip}`;
-
-      if (!isSchemaAvailable) {
-        stage = 'получаем описание..';
-      } else if (!isFreshAvailable) {
-        stage = 'получаем последние данные..';
-      }
-    } else {
-      icon = <FontAwesomeIcon icon={faTimesCircle} />;
-      classes = 'connect-icon disconnected';
-      message = 'Соединение не установлено';
-      stage = null;
+      return null;
     }
   }
 
-  return (
-    <div
-      className="connect-status-container"
-      title={title}
-      onClick={onConnectClick}
-    >
-      <span className={classes}>{icon}</span>
+  handleAnimationEnd(e) {
+    // Reset
+    if (e.animationName === 'fa-opacity-calm') {
+      this.setState({ isShouldBlink: false });
+    }
+  }
 
-      <span className="connect-message">{message}</span>
+  render() {
+    const {
+      ip,
+      lastFreshMessageTS,
+      onConnectClick,
+      isConnected,
+      isConnecting,
+      isWaitingHistory,
+      isSchemaAvailable,
+      isFreshAvailable
+    } = this.props;
 
-      <span className="connect-message">{stage}</span>
-    </div>
-  );
-};
+    let icon, classes, message, stage, title;
+
+    if (isConnected) {
+      if (isWaitingHistory) {
+        icon = <FontAwesomeIcon icon={faCircleNotch} spin />;
+        classes = 'connect-icon connected';
+        message = 'Соединено';
+        title = `IP сервера ${ip}`;
+        stage = 'Запрашиваем архивные данные с сервера..';
+      } else {
+        icon = <FontAwesomeIcon icon={faCircle} />;
+        classes = `connect-icon connected ${
+          this.state.isShouldBlink ? 'fresh' : null
+        }`;
+        message = 'Соединено';
+        title = `IP сервера ${ip}   Последнее сообщение получено в ${displayHuman(
+          lastFreshMessageTS
+        )}`;
+        stage = '';
+      }
+    } else if (!isConnected) {
+      if (isConnecting) {
+        icon = <FontAwesomeIcon icon={faCircleNotch} spin />;
+        classes = 'connect-icon connecting';
+        message = 'Соединяемся..';
+        title = `IP сервера ${ip}`;
+
+        if (!isSchemaAvailable) {
+          stage = 'получаем описание..';
+        } else if (!isFreshAvailable) {
+          stage = 'получаем последние данные..';
+        }
+      } else {
+        icon = <FontAwesomeIcon icon={faTimesCircle} />;
+        classes = 'connect-icon disconnected';
+        message = 'Соединение не установлено';
+        stage = null;
+      }
+    }
+
+    return (
+      <div
+        className="connect-status-container"
+        title={title}
+        onClick={onConnectClick}
+      >
+        <span
+          className={classes}
+          onAnimationEnd={this.handleAnimationEnd.bind(this)}
+        >
+          {icon}
+        </span>
+
+        <span className="connect-message">{message}</span>
+
+        <span className="connect-message">{stage}</span>
+      </div>
+    );
+  }
+}
+
+// export default ConnectStatus1;
+
+// const ConnectStatus = ({
+//   ip,
+//   lastFreshMessageTS,
+//   onConnectClick,
+//   isConnected,
+//   isConnecting,
+//   isWaitingHistory,
+//   isSchemaAvailable,
+//   isFreshAvailable
+// }) => {
+//   let icon, classes, message, stage, title;
+
+//   if (isConnected) {
+//     if (isWaitingHistory) {
+//       icon = <FontAwesomeIcon icon={faCircleNotch} spin />;
+//       classes = 'connect-icon connected';
+//       message = 'Соединено';
+//       title = `IP сервера ${ip}`;
+//       stage = 'Запрашиваем архивные данные с сервера..';
+//     } else {
+//       icon = <FontAwesomeIcon icon={faCircle} />;
+//       classes = 'connect-icon connected fresh';
+//       message = 'Соединено';
+//       title = `IP сервера ${ip}   Последнее сообщение получено в ${displayHuman(
+//         lastFreshMessageTS
+//       )}`;
+//       stage = '';
+//     }
+//   } else if (!isConnected) {
+//     if (isConnecting) {
+//       icon = <FontAwesomeIcon icon={faCircleNotch} spin />;
+//       classes = 'connect-icon connecting';
+//       message = 'Соединяемся..';
+//       title = `IP сервера ${ip}`;
+
+//       if (!isSchemaAvailable) {
+//         stage = 'получаем описание..';
+//       } else if (!isFreshAvailable) {
+//         stage = 'получаем последние данные..';
+//       }
+//     } else {
+//       icon = <FontAwesomeIcon icon={faTimesCircle} />;
+//       classes = 'connect-icon disconnected';
+//       message = 'Соединение не установлено';
+//       stage = null;
+//     }
+//   }
+
+//   return (
+//     <div
+//       className="connect-status-container"
+//       title={title}
+//       onClick={onConnectClick}
+//     >
+//       <span
+//         className={classes}
+//         onAnimationEnd={log.info('End')}
+//       >
+//         {icon}
+//       </span>
+
+//       <span className="connect-message">{message}</span>
+
+//       <span className="connect-message">{stage}</span>
+//     </div>
+//   );
+// };
 
 export default ConnectStatus;
